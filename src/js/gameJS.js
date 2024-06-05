8e2d09f129fa6460915795b253e4f9f0c637717b
Nikita-2406 <89524208686@mail.ru>
2024-06-05 23:51:53 +0300

    2.0.0

---
+++ src/js/gameJS.js
@@ -0,0 +1,31 @@
+class Game {
+    constructor () {
+        this.widget = document.querySelectorAll('.impactField')
+    }
+
+    randomId (last_id) {
+        const new_id = Math.floor(Math.random () * (16))
+        if (new_id === last_id) {
+            this.randomId(last_id)
+        } else {
+            return new_id
+        }
+    }
+
+    replaceField () {
+        let id = Math.floor(Math.random () * (16))
+        let lastElemId = id
+        setInterval(() => {
+            console
+            this.widget.item(lastElemId).innerHTML = `${lastElemId + 1}`
+            this.widget.item(id).innerHTML = '<img src="goblin.png" alt="">'
+            lastElemId = id
+            id = this.randomId(id)
+        }, 1000)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const widget = new Game()
+    widget.replaceField()
+})
