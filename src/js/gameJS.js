33316e0703fd541e6ce0a11c13c36ebaceb52c30
Nikita-2406 <89524208686@mail.ru>
2024-06-11 14:25:20 +0300

    3.1.0

--- src/js/gameJS.js
+++ src/js/gameJS.js
@@ -1,6 +1,25 @@
 class Game {
   constructor() {
     this.widget = document.querySelectorAll(".impactField");
+    this.image = document.querySelector("img");
+  }
+
+  addGameField() {
+    let field = document.createElement("td");
+    field.classList.add("impactField");
+    let table_row = document.createElement("tr");
+    const table = document.createElement("table");
+    const count = 0;
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        table_row.appendChild(field);
+        field = document.createElement("td");
+        field.classList.add("impactField");
+      }
+      table.appendChild(table_row);
+      table_row = document.createElement("tr");
+    }
+    document.body.appendChild(table);
   }
 
   randomId(last_id) {
@@ -13,13 +32,12 @@ class Game {
   }
 
   replaceField() {
-    const content = this.widget.item(0).innerHTML
+    this.widget = document.querySelectorAll(".impactField");
+    const content = this.image;
     let id = Math.floor(Math.random() * 16);
-    let lastElemId = 0;
     setInterval(() => {
-      this.widget.item(lastElemId).innerHTML = '';
-      this.widget.item(id).innerHTML = content;
-      lastElemId = id;
+      this.image.classList.remove("invisible");
+      this.widget.item(id).appendChild(content);
       id = this.randomId(id);
     }, 1000);
   }
@@ -27,5 +45,6 @@ class Game {
 
 document.addEventListener("DOMContentLoaded", () => {
   const widget = new Game();
+  widget.addGameField();
   widget.replaceField();
 });
